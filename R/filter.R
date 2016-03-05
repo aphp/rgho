d32ececa30d8b17ba6b6ac2c14c1f14484b421b0
afp <pierucci@gmail.com>
2016-03-05 12:08:14 +0100

    :beetle:fix filter.gho

--- R/filter.R
+++ R/filter.R
@@ -2,13 +2,13 @@
 #' @rdname get_gho_codes_
 filter_.gho <- function (.data, ..., .dots) {
   tab_attrs <- attr(.data, "attrs") %>%
-    filter(.dots = .dots)
+    filter_(.dots = .dots)
 
   codes <- unique(tab_attrs$code)
 
   build_gho(
     .data[.data %in% codes],
-    label = attr(.data),
+    labels = attr(.data),
     attrs = tab_attrs
   )
 }
