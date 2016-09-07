50f506a512c6e4bd7b6c5306d69b914b858a6379
afp <pierucci@gmail.com>
2016-09-07 23:54:05 +0200

    :wrench:started work on #19

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,21 @@ to_utf8 <- function(x) {
   x
 }
 
-clean_json <- function(x) {
+clean_json <- function(x, verbose = options()$rgho.verbose,
+                       n_print = 10) {
+  stopifnot(length(x) == 1)
+  if (verbose) {
+    pos <- gregexpr('(\\S)\\"\\"', x)[[1]]
+    if (length(pos)) {
+      message(sprintf(
+        "Corrected json errors in %i place(s):",
+        length(pos)))
+      for (i in pos) {
+        message(substr(x,
+                       max(i-n_print, 1),
+                       min(i+n_print, nchar(x))))
+      }
+    }
+  }
   gsub('(\\S)\\"\\"', '\\1\\"', x)
 }
