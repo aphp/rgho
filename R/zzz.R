b775f483d7b9e9d04fcceef3a527ba088ab3c691
Antoine Filipovic-Pierucci <pierucci@gmail.com>
2016-11-07 19:12:37 +0100

    :sparkles:add proxy option, #4

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
   op.rgho <- list(
     rgho.verbose = FALSE,
     rgho.retry = 5,
-    rgho.memotime = 3600
+    rgho.memotime = 3600,
+    rgho.http_proxy = NULL
   )
   toset <- !(names(op.rgho) %in% names(op))
   if(any(toset)) options(op.rgho[toset])
