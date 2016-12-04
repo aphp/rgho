fbee43f0d8eb1b2ced5aaf6bd4ad14d3ca44db3a
afp <pierucci@gmail.com>
2016-12-04 23:44:58 +0100

    :beetle:document all print args

--- R/man-gho.R
+++ R/man-gho.R
@@ -4,8 +4,10 @@
 #'
 #' @name man-gho
 #' @param dimension A GHO dimension.
+#' @param x Object to print.
 #' @param n Number of lines to print (if missing will print
 #'   the option \code{rgho.n} rows).
+#' @param ... Extra arguments passed to print.
 #'
 #' @return GHO dimensions/codes.
 #'
